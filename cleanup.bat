6b773db244d727ff48f35c64cee6d97e843f629b
ZakisOK <zoberbillig@gmail.com>
2026-04-10 20:33:11 +0200

    chore: remove temp commit.bat

---
+++ cleanup.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\zakob\projects\trading-floor
+git add -A
+git commit -m "chore: remove temp commit.bat"
--- commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\zakob\projects\trading-floor
-git add .
-git commit -m "feat: T0.1 - project scaffolding (pyproject, docker, Makefile, src structure, frontend skeleton)"
